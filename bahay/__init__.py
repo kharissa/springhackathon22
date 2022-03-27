47197bfe9ad7aa61ea8c6f94ef0898189ae5fd60
Kharissa Fernando Smith <kfernando@pagerduty.com>
2022-03-28 01:49:28 +0530

    update environsg

--- bahay/__init__.py
+++ bahay/__init__.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
